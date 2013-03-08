9120bf9a29d14ed54e05839d1d01d08c8f222e39
John Grossmann <JGrossmann@colgate.edu>
2013-03-08 01:53:42 -0500

    done

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -34,4 +34,49 @@ describe MoviesController do
 			end
 		end
 	end
+	describe 'listing movies on the home page' do
+		it 'should grab all ratings from movie database' do
+			Movie.should_receive(:all_ratings).and_return(["G", "PG", "PG-13", "NC-17", "R"])
+			post :index
+		end
+		describe 'session disagrees with parameters' do
+			it 'should change sort settings and redirect' do
+				session.stub(:sort).with("")
+				post :index, {:sort => "title"}
+			end
+		end
+		describe 'release date sort was clicked on' do
+			it 'should hilite release date by setting the date header to hilite' do
+				post :index, {:sort => 'release_date'}
+			end
+		end
+	end
+	describe 'going to a movies info page' do
+		it 'should return the movies information' do
+			fake_movie = mock("movie1")
+			Movie.should_receive(:find).with("1").and_return(fake_movie)
+			post :show, {:id => 1}
+		end
+	end
+	describe 'creating a new movie' do
+		it 'should save a movie to database' do
+			fake_movie = mock("movie1")
+			Movie.stub(:create).and_return(fake_movie)
+			post :create, {:title => 'stupid movie', :id => 1}
+		end
+	end
+	describe 'updating a movie' do
+		it 'should update a movie in the database' do
+			fake_movie = FactoryGirl.build(:movie)
+			Movie.should_receive(:find).with("#{fake_movie.id}").and_return(fake_movie)
+			post :update, {:id => fake_movie.id}
+		end
+	end
+	describe 'editing a movie info' do
+		it 'should edit the movies info in the database' do
+			fake_movie = FactoryGirl.build(:movie)
+			Movie.should_receive(:find).with("#{fake_movie.id}").and_return(fake_movie)
+			post :edit, {:id => fake_movie.id}
+		end
+	end
 end
